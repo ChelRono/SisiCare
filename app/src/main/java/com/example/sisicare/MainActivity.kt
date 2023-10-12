8ab531a2129db3ebe4872152a7d80ed8a199b7bf
Valarie <Valarierono97@gmail.com>
2023-10-12 12:29:16 +0300

    Creates folder for all the screens required

---
+++ app/src/main/java/com/example/sisicare/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.sisicare
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.sisicare.ui.theme.SisiCareTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SisiCareTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                 MainContent()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainContent() {
+    Text(
+        text = "Hello",
+    )
+}
+
---
+++ app/src/main/java/com/example/sisicare/screen/navigation/NavigationScreen.kt
@@ -0,0 +1,4 @@
+package com.example.sisicare.screen.navigation
+
+class NavigationScreen {
+}
