885b10962d6267fa6403c053b106f0aad986fdc9
Valarie <Valarierono97@gmail.com>
2023-10-18 12:12:14 +0300

    Adds text to the book screen

--- app/src/main/java/com/example/sisicare/screen/books/BookScreen.kt
+++ app/src/main/java/com/example/sisicare/screen/books/BookScreen.kt
@@ -1,2 +1,15 @@
 package com.example.sisicare.screen.books
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.sisicare.screen.home.HomeScreenViewModel
+
+
+@Composable
+fun BookScreen(
+    viewModel: BookScreenViewModel,
+    navController: NavController
+){
+    Text(text = "Book")
+}
