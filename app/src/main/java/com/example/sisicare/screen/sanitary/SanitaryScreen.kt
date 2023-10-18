4fc70cf45ff8f35bb0d35a66d76389cf669ad1c3
Valarie <Valarierono97@gmail.com>
2023-10-18 12:13:06 +0300

    Adds text to the sanitary screen

--- app/src/main/java/com/example/sisicare/screen/sanitary/SanitaryScreen.kt
+++ app/src/main/java/com/example/sisicare/screen/sanitary/SanitaryScreen.kt
@@ -1,2 +1,15 @@
 package com.example.sisicare.screen.sanitary
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.sisicare.screen.home.HomeScreenViewModel
+
+
+@Composable
+fun SanitaryScreen(
+    viewModel: SanitaryScreenViewModel,
+    navController: NavController
+){
+    Text(text = "Sanitary")
+}
