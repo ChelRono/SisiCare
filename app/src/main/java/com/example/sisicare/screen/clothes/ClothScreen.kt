0a570718306b4f4c8563730aa3faaae09b70673a
Valarie <Valarierono97@gmail.com>
2023-10-18 12:11:01 +0300

    Adds text to the cloth screen

--- app/src/main/java/com/example/sisicare/screen/clothes/ClothScreen.kt
+++ app/src/main/java/com/example/sisicare/screen/clothes/ClothScreen.kt
@@ -1,2 +1,15 @@
 package com.example.sisicare.screen.clothes
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.sisicare.screen.home.HomeScreenViewModel
+
+
+@Composable
+fun ClothScreen(
+    viewModel: ClothScreenViewModel,
+    navController: NavController
+){
+    Text(text = "Cloth")
+}
