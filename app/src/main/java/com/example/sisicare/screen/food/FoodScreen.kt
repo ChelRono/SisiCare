1bd279388ba77c69e71cf23f32a2b7498a58ebba
Valarie <Valarierono97@gmail.com>
2023-10-18 12:09:15 +0300

    Adds text to the food screen

--- app/src/main/java/com/example/sisicare/screen/food/FoodScreen.kt
+++ app/src/main/java/com/example/sisicare/screen/food/FoodScreen.kt
@@ -1,2 +1,15 @@
 package com.example.sisicare.screen.food
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.sisicare.screen.home.HomeScreenViewModel
+
+
+@Composable
+fun FoodScreen(
+    viewModel: FoodScreenViewModel,
+    navController: NavController
+){
+    Text(text = "Food")
+}
