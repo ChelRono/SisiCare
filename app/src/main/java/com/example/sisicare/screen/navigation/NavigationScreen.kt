97b158981a34882d02751628309c418929d8bf38
Valarie <Valarierono97@gmail.com>
2023-10-18 11:57:52 +0300

    Adds sealed class for the bottom nav items

--- app/src/main/java/com/example/sisicare/screen/navigation/NavigationScreen.kt
+++ app/src/main/java/com/example/sisicare/screen/navigation/NavigationScreen.kt
@@ -1,4 +1,13 @@
 package com.example.sisicare.screen.navigation
 
-class NavigationScreen {
+import com.example.sisicare.R
+
+
+sealed class BottomNavItem(var title:String, var icon:Int, var screen_route:String){
+
+    object Home : BottomNavItem("Home", R.drawable.home,"home")
+    object Food: BottomNavItem("Food",R.drawable.food,"food")
+    object Books: BottomNavItem("Books",R.drawable.library_books,"books")
+    object Sanitary: BottomNavItem("Sanitary", R.drawable.female,"sanitary")
+    object Clothes: BottomNavItem("Clothes",R.drawable.shopping_basket,"clothes")
 }
