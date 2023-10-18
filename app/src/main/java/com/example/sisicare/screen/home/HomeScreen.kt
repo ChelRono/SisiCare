7d74c9c3b29210cbd6bb299e8d867f78c43cd289
Valarie <Valarierono97@gmail.com>
2023-10-18 12:08:12 +0300

    Adds text to the home screen

--- app/src/main/java/com/example/sisicare/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/sisicare/screen/home/HomeScreen.kt
@@ -1,1 +1,14 @@
 package com.example.sisicare.screen.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(
+    viewModel: HomeScreenViewModel,
+    navController: NavController
+  ){
+     Text(text = "Home")
+}
