7a3a4367802c23d2841c8df5ba38cb20dd71416c
Valarie <Valarierono97@gmail.com>
2023-11-06 22:45:43 +0300

    Initial commit on forgot password screen

--- app/src/main/java/com/example/sisicare/MainActivity.kt
+++ app/src/main/java/com/example/sisicare/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.sisicare.screen.forgotpassword.ForgotPasswordDetails
 import com.example.sisicare.ui.theme.SisiCareTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,17 +20,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                 MainContent()
+                 ForgotPasswordDetails()
                 }
             }
         }
     }
 }
 
-@Composable
-fun MainContent() {
-    Text(
-        text = "Hello",
-    )
-}
 
--- app/src/main/java/com/example/sisicare/screen/forgotpassword/ForgotPasswordScreen.kt
+++ app/src/main/java/com/example/sisicare/screen/forgotpassword/ForgotPasswordScreen.kt
@@ -1,2 +1,12 @@
 package com.example.sisicare.screen.forgotpassword
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ForgotPasswordDetails() {
+    Text(
+        text = "Forgot Password screen",
+    )
+}
+
