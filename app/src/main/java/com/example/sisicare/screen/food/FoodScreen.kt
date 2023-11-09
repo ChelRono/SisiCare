85cbd204f1dc9a84a13b09a56cf4ca3376e0529c
Valarie <Valarierono97@gmail.com>
2023-11-09 23:37:56 +0300

    Adds cards to display the food banks details

--- app/src/main/java/com/example/sisicare/screen/food/FoodScreen.kt
+++ app/src/main/java/com/example/sisicare/screen/food/FoodScreen.kt
@@ -1,19 +1,26 @@
 package com.example.sisicare.screen.food
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
@@ -23,8 +30,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -39,6 +48,7 @@ fun FoodScreen() {
     Column(
         modifier = Modifier
             .padding(20.dp)
+            .background(Color(0xFFFAF9F6))
     ) {
         Row {
             Icon(
@@ -100,16 +110,85 @@ fun OrganisationList() {
     )
     Text(
         text = "Organisations near you",
-        fontSize = 22.sp,
+        fontSize = 26.sp,
         fontWeight = FontWeight.Bold
     )
-    Spacer(modifier = Modifier.height(15.dp))
 
     LazyHorizontalGrid(
-        rows = GridCells.Fixed(1)
+        rows = GridCells.Fixed(1),
+        horizontalArrangement = Arrangement.spacedBy(30.dp)
     ) {
         items(listOfItems) { item ->
-
+            OrganisationDetails(title = item)
         }
     }
 }
+
+@Composable
+fun OrganisationDetails(title: String) {
+    ElevatedCard (
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+        ),
+        elevation = CardDefaults.cardElevation(6.dp),
+        modifier = Modifier
+            .size(240.dp)
+            .requiredHeight(370.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.foodbank),
+            contentDescription = "",
+            modifier = Modifier,
+            contentScale = ContentScale.FillWidth
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(
+            text = title,
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+        )
+        Row {
+           Icon(
+               painter = painterResource(id = R.drawable.location_on_24) ,
+               contentDescription = "location",
+               tint = Color(0xFFfb8500),
+               modifier = Modifier
+                   .padding(horizontal = 16.dp)
+               )
+           Text(
+               text = "Los Angeles, California",
+               color = Color.LightGray,
+               fontSize = 16.sp
+           )
+       }
+        Text(
+            text = "This center is a food bank that accepts dry foods and money to buy food",
+            modifier = Modifier
+                .padding(horizontal = 16.dp),
+            color = Color.LightGray,
+            fontSize = 16.sp
+        )
+        Spacer(modifier = Modifier.height(14.dp))
+
+        Button(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .height(40.dp)
+                ,
+            onClick = {},
+            shape = RoundedCornerShape(12.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFfb8500))
+        ) {
+            Text(
+                text = "DONATE ",
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp
+            )
+        }
+
+        }
+    }
+
