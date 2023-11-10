ba1847466d9eff0ee78f90845b7a17605c6da355
Valarie <Valarierono97@gmail.com>
2023-11-10 20:42:37 +0300

    Adds card details

--- app/src/main/java/com/example/sisicare/screen/food/FoodScreen.kt
+++ app/src/main/java/com/example/sisicare/screen/food/FoodScreen.kt
@@ -1,22 +1,30 @@
 package com.example.sisicare.screen.food
 
+import android.provider.ContactsContract.CommonDataKinds.Organization
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CardDefaults
@@ -45,10 +53,11 @@ import com.example.sisicare.R
 fun FoodScreen() {
     var organisation by remember { mutableStateOf("") }
 
+
     Column(
         modifier = Modifier
             .padding(20.dp)
-            .background(Color(0xFFFAF9F6))
+            .fillMaxSize()
     ) {
         Row {
             Icon(
@@ -97,6 +106,11 @@ fun FoodScreen() {
         Spacer(modifier = Modifier.height(20.dp))
 
         OrganisationList()
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OrganizationCard()
+
     }
 }
 
@@ -109,86 +123,124 @@ fun OrganisationList() {
         "City Harvest"
     )
     Text(
-        text = "Organisations near you",
-        fontSize = 26.sp,
-        fontWeight = FontWeight.Bold
+            text = "Organisations near you",
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Bold,
     )
-
     LazyHorizontalGrid(
         rows = GridCells.Fixed(1),
-        horizontalArrangement = Arrangement.spacedBy(30.dp)
-    ) {
+        horizontalArrangement = Arrangement.spacedBy(30.dp),
+        modifier = Modifier
+            .padding(vertical = 10.dp)
+        ) {
         items(listOfItems) { item ->
-            OrganisationDetails(title = item)
+
+            CardDetails(title = item)
+
         }
     }
 }
 
 @Composable
-fun OrganisationDetails(title: String) {
-    ElevatedCard (
-        colors = CardDefaults.cardColors(
-            containerColor = Color.White,
-        ),
-        elevation = CardDefaults.cardElevation(6.dp),
+fun CardDetails(title: String) {
+    Column (
         modifier = Modifier
-            .size(240.dp)
-            .requiredHeight(370.dp)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.foodbank),
-            contentDescription = "",
-            modifier = Modifier,
-            contentScale = ContentScale.FillWidth
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Text(
-            text = title,
-            fontSize = 22.sp,
-            fontWeight = FontWeight.Bold,
+    ){
+        ElevatedCard (
+            colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+                ),
+            elevation = CardDefaults.cardElevation(6.dp),
             modifier = Modifier
-                .padding(horizontal = 16.dp)
-        )
-        Row {
-           Icon(
-               painter = painterResource(id = R.drawable.location_on_24) ,
-               contentDescription = "location",
-               tint = Color(0xFFfb8500),
-               modifier = Modifier
-                   .padding(horizontal = 16.dp)
-               )
-           Text(
-               text = "Los Angeles, California",
-               color = Color.LightGray,
-               fontSize = 16.sp
-           )
-       }
-        Text(
-            text = "This center is a food bank that accepts dry foods and money to buy food",
-            modifier = Modifier
-                .padding(horizontal = 16.dp),
-            color = Color.LightGray,
-            fontSize = 16.sp
-        )
-        Spacer(modifier = Modifier.height(14.dp))
-
-        Button(
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .height(40.dp)
-                ,
-            onClick = {},
-            shape = RoundedCornerShape(12.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFfb8500))
+                .size(width = 240.dp, height = 370.dp)
         ) {
-            Text(
-                text = "DONATE ",
-                fontWeight = FontWeight.Bold,
-                fontSize = 16.sp
+            Image(
+                painter = painterResource(id = R.drawable.foodbank),
+                contentDescription = "",
+                modifier = Modifier,
+                contentScale = ContentScale.FillWidth
             )
-        }
+            Spacer(modifier = Modifier.height(10.dp))
 
+            Text(
+                text = title,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+            )
+            Row {
+                Icon(
+                   painter = painterResource(id = R.drawable.location_on_24) ,
+                   contentDescription = "location",
+                   tint = Color(0xFFfb8500),
+                   modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                )
+                Text(
+                   text = "Los Angeles, California",
+                   color = Color.Gray,
+                   fontSize = 16.sp
+                )
+            }
+            Text(
+                text = "This center is a food bank that accepts dry foods and money to buy food",
+                modifier = Modifier
+                    .padding(horizontal = 16.dp),
+                color = Color.Gray,
+                fontSize = 16.sp,
+            )
+            Spacer(modifier = Modifier.height(14.dp))
+
+            Button(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .height(40.dp),
+                onClick = {},
+                shape = RoundedCornerShape(12.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFfb8500))
+            ) {
+                Text(
+                    text = "DONATE ",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            }
         }
     }
+}
 
+@Composable
+fun OrganizationCard() {
+    Column {
+        Text(
+            text = "Donate Again",
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        ElevatedCard(
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White,
+            ),
+            elevation = CardDefaults.cardElevation(6.dp),
+            modifier = Modifier
+                .size(240.dp)
+        ) {
+            Column {
+                Image(
+                    painter = painterResource(id = R.drawable.foodbank),
+                    contentDescription = "",
+                    contentScale = ContentScale.FillBounds
+                )
+            }
+        }
+        Text(
+            text = "The Food Centre",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(vertical = 10.dp)
+
+        )
+    }
+}
