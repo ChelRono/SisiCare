83e8a9f41122422eb6ef596710b326fb2fd2f739
Valarie <Valarierono97@gmail.com>
2023-11-14 20:44:59 +0300

    Separates the two boxes

--- app/src/main/java/com/example/sisicare/screen/clothes/ClothScreen.kt
+++ app/src/main/java/com/example/sisicare/screen/clothes/ClothScreen.kt
@@ -3,29 +3,93 @@ package com.example.sisicare.screen.clothes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.FloatingActionButtonElevation
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ClothesScreen(){
     Column {
         Box(
             modifier = Modifier
-                .background(Color.Red)
+                .background(Color(0xFFfb8500))
+                .fillMaxWidth()
+                .weight(0.2F),
+            contentAlignment = Alignment.Center
         ) {
             Text(
-                text = "Box 1",
+                text = "Clothes donation",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
             )
         }
         Box(
             modifier = Modifier
-                .background(Color.Blue)
+                .padding(24.dp)
+                .fillMaxWidth()
+                .weight(1F),
         ) {
-            Text(
-                text = "Box 2",
-            )
+            ElevatedCard(
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 6.dp
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp),
+                ) {
+                    Text(
+                        text = "Name : Valarie Rono",
+                        textAlign = TextAlign.Center,
+                    )
+                    Text(
+                        text = "Location : Annex",
+                        textAlign = TextAlign.Center,
+                    )
+                    Text(
+                        text = "Date : 21/03/2023",
+                        textAlign = TextAlign.Center,
+                    )
+
+                }
+
+            }
         }
     }
 }
+
+@Composable
+fun DonateButton(){
+    ExtendedFloatingActionButton(
+        onClick = {  },
+        shape = RoundedCornerShape(12.dp),
+        containerColor = Color(0xFFfb8500),
+        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(5.dp)
+
+    ) {
+        Icon(Icons.Filled.Add, "Floating action button.")
+        Text(text = "DONATE")
+    }
+}
+
