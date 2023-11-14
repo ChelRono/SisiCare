7f5bd6741a16362bc321c16a7855e7576193463e
Valarie <Valarierono97@gmail.com>
2023-11-14 20:14:01 +0300

    Initial commit on clothes screen

--- app/src/main/java/com/example/sisicare/screen/clothes/ClothScreen.kt
+++ app/src/main/java/com/example/sisicare/screen/clothes/ClothScreen.kt
@@ -1,2 +1,31 @@
 package com.example.sisicare.screen.clothes
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun ClothesScreen(){
+    Column {
+        Box(
+            modifier = Modifier
+                .background(Color.Red)
+        ) {
+            Text(
+                text = "Box 1",
+            )
+        }
+        Box(
+            modifier = Modifier
+                .background(Color.Blue)
+        ) {
+            Text(
+                text = "Box 2",
+            )
+        }
+    }
+}
