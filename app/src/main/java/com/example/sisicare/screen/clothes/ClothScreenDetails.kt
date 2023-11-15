56e03d2fb6b79973ed8d428d0b52fd01176a45aa
Valarie <Valarierono97@gmail.com>
2023-11-15 16:14:30 +0300

    Adds the quantity category

--- app/src/main/java/com/example/sisicare/screen/clothes/ClothScreenDetails.kt
+++ app/src/main/java/com/example/sisicare/screen/clothes/ClothScreenDetails.kt
@@ -2,7 +2,9 @@ package com.example.sisicare.screen.clothes
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.DropdownMenu
@@ -32,6 +34,10 @@ fun ClothScreenDetails(){
             .padding(20.dp)
     ){
         CategoryDetails()
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        QuantityDetails()
     }
 }
 @Composable
@@ -52,7 +58,7 @@ fun CategoryDetails() {
             .fillMaxWidth()
             .onGloballyPositioned { coordinates ->
                 textFieldSize = coordinates.size.toSize()
-                },
+            },
         label = { Text("Category") },
         trailingIcon = {
             Icon(icon, "contentDescription",
@@ -74,6 +80,53 @@ fun CategoryDetails() {
                 }
         }
     }
+}
+
+@Composable
+fun QuantityDetails(){
+    var expanded by remember { mutableStateOf(false) }
+    val quantity = listOf("1", "2","3","4","5","6","7","8","9","10","10+", )
+    var selectedText by remember { mutableStateOf("") }
+    var textFieldSize by remember { mutableStateOf(Size.Zero)}
+    val icon = if (expanded)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
+
+    OutlinedTextField(
+        value = selectedText,
+        onValueChange = { selectedText = it },
+        modifier = Modifier
+            .fillMaxWidth()
+            .onGloballyPositioned { coordinates ->
+                textFieldSize = coordinates.size.toSize()
+            },
+        label = { Text("Quantity") },
+        trailingIcon = {
+            Icon(icon, "contentDescription",
+                Modifier.clickable { expanded = !expanded })
+        }
+    )
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = { expanded = false },
+        modifier = Modifier
+            .width(with(LocalDensity.current) { textFieldSize.width.toDp() })
+    ) {
+        quantity.forEach { label ->
+            DropdownMenuItem(onClick = {
+                selectedText = label
+                expanded = false
+            }) {
+                Text(text = label)
+            }
+        }
+    }
+}
+
+@Composable
+fun PhoneDetails(){
+
 }
 
 
