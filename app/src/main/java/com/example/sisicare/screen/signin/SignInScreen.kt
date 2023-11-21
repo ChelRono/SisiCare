651f68cff2ab16c7a8d08c40dbd8b672d9d5bd6c
Valarie <Valarierono97@gmail.com>
2023-11-21 16:15:56 +0300

    sign in screen

--- app/src/main/java/com/example/sisicare/screen/signin/SignInScreen.kt
+++ app/src/main/java/com/example/sisicare/screen/signin/SignInScreen.kt
@@ -4,13 +4,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
@@ -23,8 +28,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.sisicare.R
@@ -33,6 +40,8 @@ import com.example.sisicare.R
 @Composable
 fun SignInDetails() {
     var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var showPassword by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
@@ -51,7 +60,7 @@ fun SignInDetails() {
 
         OutlinedTextField(
             value = username,
-            onValueChange = {newValue ->
+            onValueChange = { newValue ->
                 username = newValue
             },
             modifier = Modifier
@@ -75,7 +84,102 @@ fun SignInDetails() {
                 )
             }
         )
+        Spacer(modifier = Modifier.height(15.dp))
 
+        OutlinedTextField(
+            value = password,
+            onValueChange = { newValue ->
+                password = newValue
+            },
+            modifier = Modifier
+                .border(2.dp, Color.Gray, RoundedCornerShape(30.dp))
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(30.dp),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color(0xFFfb8500),
+                unfocusedBorderColor = Color.DarkGray
+            ),
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.lock_24),
+                    contentDescription = ""
+                )
+            },
+            trailingIcon = {
+                if (showPassword) {
+                    IconButton(onClick = { showPassword = false }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.visibility_24),
+                            contentDescription = ""
+                        )
+                    }
+                } else {
+                    IconButton(
+                        onClick = { showPassword = true }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.visibility_off_24),
+                            contentDescription = ""
+                        )
+                    }
+                }
+            },
+            placeholder = {
+                Text(
+                    text = "Password",
+                    color = Color.Gray
+                )
+            },
+            visualTransformation = if (showPassword) {
+                VisualTransformation.None
+            } else {
+                PasswordVisualTransformation()
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Text(
+            text = "Forgot Password?",
+            fontSize = 14.sp,
+            modifier = Modifier
+                .align(Alignment.End),
+            color = Color.Gray
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Button(
+            modifier = Modifier
+                .height(50.dp)
+                .fillMaxWidth(),
+            onClick = {},
+            shape = RoundedCornerShape(30.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFfb8500))
+        ) {
+            Text(
+                text = "Sign In",
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp
+            )
+        }
+    }
+    Column(
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row {
+            Text(
+                text = "Don't have an account?",
+                modifier = Modifier
+                    .padding(vertical = 24.dp)
+            )
+            Text(
+                text = "Sign Up",
+                color = Color.Blue,
+                modifier = Modifier
+                    .padding(vertical = 24.dp)
+            )
+        }
     }
 }
+
 
