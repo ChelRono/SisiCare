5920d4f340bb04ba95a87c82481a35c4b00ec754
Valarie <Valarierono97@gmail.com>
2023-11-21 16:15:36 +0300

    sign in screen

--- app/src/main/java/com/example/sisicare/screen/signin/SignInScreen.kt
+++ app/src/main/java/com/example/sisicare/screen/signin/SignInScreen.kt
@@ -1,12 +1,81 @@
 package com.example.sisicare.screen.signin
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.sisicare.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignInDetails() {
-    Text(
-        text = "Sign In Screen",
-    )
+    var username by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "Sign In",
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFFfb8500)
+        )
+        Spacer(modifier = Modifier.height(25.dp))
+
+        OutlinedTextField(
+            value = username,
+            onValueChange = {newValue ->
+                username = newValue
+            },
+            modifier = Modifier
+                .border(2.dp, Color.Gray, RoundedCornerShape(30.dp))
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(30.dp),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color(0xFFfb8500),
+                unfocusedBorderColor = Color.DarkGray
+            ),
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.person_24),
+                    contentDescription = ""
+                )
+            },
+            placeholder = {
+                Text(
+                    text = "Name or Email",
+                    color = Color.Gray
+                )
+            }
+        )
+
+    }
 }
 
